002996dcabbda599faf2a0d035a2fd759e2f8112
jun9708 <wnsgud1997@gmail.com>
2024-04-19 12:28:52 +0900

    판매자 회원가입 유효성검사 기능 작성

--- src/main/java/kr/co/lotteon/repository/SellerRepository.java
+++ src/main/java/kr/co/lotteon/repository/SellerRepository.java
@@ -4,7 +4,14 @@ import kr.co.lotteon.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SellerRepository extends JpaRepository<Seller, String> {
+    public Optional<Seller> findBySellerName(String sellerName);
+    public Optional<Seller> findByCompany(String company);
+    public Optional<Seller> findByLicenseNum(String licenseNum );
+    public Optional<Seller> findByBusinessNum(String businessNum);
+    public Optional<Seller> findBySellerHp(String sellerHp);
 
 }
--- src/main/java/kr/co/lotteon/repository/UserRepository.java
+++ src/main/java/kr/co/lotteon/repository/UserRepository.java
@@ -12,4 +12,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, String>, UserRepositoryCustom {
     public Optional<User> findByUserHp(String userHp);
     public Optional<User> findByUserEmail(String Email);
+
 }
