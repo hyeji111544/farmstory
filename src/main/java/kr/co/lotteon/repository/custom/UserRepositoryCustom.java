bfdfdb25733d0be39b60cf913cdd214103c8782f
gudals9677 <gudals9677@gmail.com>
2024-04-27 18:13:58 +0900

    fid,fpw

--- src/main/java/kr/co/lotteon/repository/custom/UserRepositoryCustom.java
+++ src/main/java/kr/co/lotteon/repository/custom/UserRepositoryCustom.java
@@ -4,10 +4,18 @@ import kr.co.lotteon.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepositoryCustom {
     
     // 마이페이지 출력을 위해 user_id로 유저 정보 조회
     public User selectUserInfo(String userId);
+
+    // userId 찾기
+    Optional<User> findUserIdByUserNameAndUserEmail(String userName, String userEmail);
+
+    // userPw 수정
+    public long updateUserPwByUserIdAndUserEmail(String userId,String userPw, String userEmail);
 }
--- src/main/java/kr/co/lotteon/repository/impl/UserRepositoryImpl.java
+++ src/main/java/kr/co/lotteon/repository/impl/UserRepositoryImpl.java
@@ -7,6 +7,9 @@ import kr.co.lotteon.repository.custom.UserRepositoryCustom;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,10 +19,39 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
     private QUser qUser = QUser.user;
 
     // 마이페이지 출력을 위해 user_id로 유저 정보 조회
-    public User selectUserInfo(String userId){
-         return jpaQueryFactory
-                    .selectFrom(qUser)
-                    .where(qUser.userId.eq(userId))
-                    .fetchOne();
-    };
+    public User selectUserInfo(String userId) {
+        return jpaQueryFactory
+                .selectFrom(qUser)
+                .where(qUser.userId.eq(userId))
+                .fetchOne();
+    }
+
+    // UserId 찾기
+    public Optional<User> findUserIdByUserNameAndUserEmail(String userName, String userEmail) {
+        User user = jpaQueryFactory
+                .selectFrom(qUser)
+                .where(qUser.userName.eq(userName)
+                        .and(qUser.userEmail.eq(userEmail)))
+                .fetchOne();
+
+        return Optional.ofNullable(user);
+    }
+
+    // UserPw update
+    @Transactional
+    public long updateUserPwByUserIdAndUserEmail(String userId, String userPw, String userEmail) {
+        try {
+            long result = jpaQueryFactory
+                    .update(qUser)
+                    .set(qUser.userPw, userPw)
+                    .where(qUser.userId.eq(userId)
+                            .and(qUser.userEmail.eq(userEmail)))
+                    .execute();
+            log.info("impl : " + result);
+            return result;
+        } catch (Exception e) {
+            log.error("Error msg :" + e.getMessage());
+            return -1;
+        }
+    }
 }
