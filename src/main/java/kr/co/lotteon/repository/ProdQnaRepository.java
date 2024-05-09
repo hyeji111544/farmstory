2ac8a63a64c445b69b6196754f96c9d2f1071a1f
NAIMJAE <153374452+NAIMJAE@users.noreply.github.com>
2024-05-09 12:51:57 +0900

    asasda

--- src/main/java/kr/co/lotteon/repository/ProdQnaRepository.java
+++ src/main/java/kr/co/lotteon/repository/ProdQnaRepository.java
@@ -1,12 +1,16 @@
 package kr.co.lotteon.repository;
 
+import com.querydsl.core.Tuple;
+import kr.co.lotteon.dto.PageRequestDTO;
 import kr.co.lotteon.entity.ProdQna;
 import kr.co.lotteon.repository.custom.ProdQnaRepositoryCustom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdQnaRepository extends JpaRepository<ProdQna, Integer>, ProdQnaRepositoryCustom {
-
-
+    // 상품 문의 조회
+    public Page<Tuple> selectProdQna(int prodNo, Pageable pageable, PageRequestDTO qnaPageRequestDTO);
 }
